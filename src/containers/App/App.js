e83da20e97f85319dded00053b0409c5fa3761f2
Eric Mellow <ericmellow@gmail.com>
2018-07-18 16:05:08 -0600

    Add initial structure of Sidebar container

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Sidebar from '../Sidebar/Sidebar'
 import './App.css';
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         REACT YO FACE!
+        <Sidebar />
       </div>
     );
   }
---
+++ src/containers/Sidebar/Sidebar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Sidebar.css';
+
+class Sidebar extends Component {
+  render() {
+    return (
+      <div className="header">
+        <h1>Leaderboard</h1>
+        <section className="main">
+          <h3>Main</h3>
+          <button>Events</button>
+          <button>Riders</button>
+          <button>Media</button>
+        </section>
+        <button>Admin Login</button>
+      </div>
+    )
+  }
+}
+
+export default Sidebar;
