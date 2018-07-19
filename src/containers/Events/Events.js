1b147d98be8190bf6d606bb6b7234cac42357bb4
Tony Robertson <tonyr729@gmail.com>
2018-07-19 17:27:22 -0600

    Add and connected socket.io

--- src/containers/Events/Events.js
+++ src/containers/Events/Events.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
+import { subscribeToTimer } from '../../timer';
 import './Events.css';
 
 class Events extends Component {
+  constructor() {
+    super();
+  };
+
+  // componentDidMount(){
+  //   subscribeToTimer((err, timestamp) => console.log(timestamp))
+  // }
+
   render() {
     return (
       <div className="events">
