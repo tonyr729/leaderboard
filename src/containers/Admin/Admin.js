a990a724816a50e7376fa1f62f3cda697c26eecd
Tony Robertson <tonyr729@gmail.com>
2018-07-21 15:37:50 -0600

    Add form details and values connected to state

--- src/containers/Admin/Admin.js
+++ src/containers/Admin/Admin.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 import './Admin.css';
 
 class Admin extends Component {
   constructor() {
     super();
     this.state = {
-      riderName: '',
-      riderDivision: ''
+      runOne: '',
+      runTwo: '',
+      runThree: ''
     }
   };
 
-  submitChanges = () => {
+  generateRiders = () => {
+
+  }
+
+  handleFormChange = (event) => {
+    const {name, value} = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  submitChanges = (event) => {
+    event.preventDefault();
     console.log(this.state)
   }
  
@@ -18,23 +33,49 @@ class Admin extends Component {
     return (
       <div className="admin">
         <div className="admin-update">
-          <form id="updateForm">
-            <input type="text" 
-              onChange={this.handleFormChange}
-              value={this.state.riderName}
-              name="riderName"
-              className='admin-input'/>
-            <input type="text" 
-              onChange={this.handleFormChange}
-              value={this.state.riderDivision}
-              name="riderDivision"
-              className='admin-input'/>
-            <button onSubmit={this.submitChanges}className="form-submit">Update</button>
+          <label for="rider-names" className="choose-rider">Choose Rider
+            <select id="rider-names" className="rider-names">
+              <option value="">Choose a rider</option>
+            </select>
+          </label>
+          <form id="updateForm" onSubmit={this.submitChanges}>
+            <label className='runone-label'>
+              Run 1:
+              <input type="text" 
+                onChange={this.handleFormChange}
+                value={this.state.runOne}
+                name="runOne"
+                className='admin-input'/>
+            </label>
+            <label className='runtwo-label'>
+              Run 2:
+              <input type="text" 
+                onChange={this.handleFormChange}
+                value={this.state.runTwo}
+                name="runTwo"
+                className='admin-input'/>
+            </label>
+            <label className='runthree-label'>
+              Run 3:
+              <input type="text" 
+                onChange={this.handleFormChange}
+                value={this.state.runThree}
+                name="runThree"
+                className='admin-input'/>
+            </label>
+            <input type="submit" value="Update" className="form-submit" />
           </form>
+        </div>
+        <div className="add-media">
+          <p className="coming-soon">Feature Coming Soon</p>
         </div>
       </div>
     );
   }
 }
 
-export default Admin;
+export const mapDispatchToProps = (dispatch) => ({
+  addAllEvents: (events) => dispatch(addEvents(events))
+});
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Admin));
