71d96c7d0e2aa64c81ec0a8db1c37ee5296b9cd1
Tony Robertson <tonyr729@gmail.com>
2018-07-21 12:29:43 -0600

    Add test code for websocket

--- src/timer.js
+++ src/timer.js
@@ -1,9 +1,9 @@
 import openSocket from 'socket.io-client';
 const  socket = openSocket('http://localhost:3001');
 
-function subscribeToTimer(cb) {
-  socket.on('timer', timestamp => cb(null, timestamp));
-  socket.emit('subscribeToTimer', 1000);
+function subscribeToChange(cb, change) {
+  socket.on('change', changeAfter => cb(null, changeAfter));
+  socket.emit('subscribeToChange', change);
 }
 
-export { subscribeToTimer };
+export { subscribeToChange };
