be83e765911c1142a772bfade0e23f80d5f6668b
Tony Robertson <tonyr729@gmail.com>
2018-07-28 13:38:07 -0600

    Update path and snapshot for SidebarFull component

--- src/containers/SidebarFull/SidebarFull.test.js
+++ src/containers/SidebarFull/SidebarFull.test.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { shallow } from "enzyme";
-import Sidebar from "./Sidebar";
+import SidebarFull from "./SidebarFull";
 
-describe('Sidebar', () => {
+describe('SidebarFull', () => {
   it('should match the snapshot', () => {
-    const wrapper = shallow(<Sidebar />);
+    const wrapper = shallow(<SidebarFull />);
     expect(wrapper).toMatchSnapshot();
   });
 });
