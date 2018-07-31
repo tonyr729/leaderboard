3b4e007e34e0edd57610ae7536ad1278a847338f
Eric Mellow <ericmellow@gmail.com>
2018-07-31 14:33:37 -0600

    Add button click test for Sidebar component

--- src/containers/Sidebar/Sidebar.test.js
+++ src/containers/Sidebar/Sidebar.test.js
@@ -13,6 +13,14 @@ describe('Sidebar', () => {
     expect(wrapper.state('collapsed')).toEqual(false)
   });
 
+  it('should call toggleSidebar when the button is clicked', () => {
+    const wrapper = shallow(<Sidebar />);
+    const spy = jest.spyOn(wrapper.instance(), 'toggleSidebar');
+    
+    wrapper.find('.icon-left-dir').simulate('click');
+    expect(spy).toHaveBeenCalled()
+  });
+
   describe('toggleSidebar', () => {
     it('should change the value of collapsed in state to the opposite value', () => {
       const wrapper = shallow(<Sidebar />);
